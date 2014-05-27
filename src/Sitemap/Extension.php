d0969ef70275dd2e3c3755d80b0b0c861284d522
Vít Kutný <vitkutny@outlook.com>
2014-05-27 02:50:39 +0200

    dev

--- src/Sitemap/Extension.php
+++ src/Sitemap/Extension.php
@@ -5,5 +5,17 @@ namespace WebEdit\Sitemap;
 use WebEdit;
 
 final class Extension extends WebEdit\Extension {
-    
+
+    public function loadConfiguration() {
+        $builder = $this->getContainerBuilder();
+        $builder->addDefinition($this->prefix('repository'))
+                ->setClass('WebEdit\Sitemap\Repository');
+        $builder->addDefinition($this->prefix('facade'))
+                ->setClass('WebEdit\Sitemap\Facade');
+        $builder->addDefinition($this->prefix('control'))
+                ->setImplement('WebEdit\Sitemap\Control\Factory');
+        $builder->addDefinition($this->prefix('form.control'))
+                ->setImplement('WebEdit\Sitemap\Form\Control\Factory');
+    }
+
 }

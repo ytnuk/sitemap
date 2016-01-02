43c080e2113d4bbf8751f78e35c9174a3e91baf8
Vít Kutný <vitkutny@icloud.com>
2016-01-02 19:35:57 +0100

    code style: do not wrap call parameters

--- src/Sitemap/Xml/Presenter.php
+++ src/Sitemap/Xml/Presenter.php
@@ -46,29 +46,15 @@ final class Presenter
 		parent::__construct();
 		$this->model = $model;
 		$this->linkRepository = $linkRepository;
-		$this->document = new DOMDocument(
-			'1.0',
-			'UTF-8'
-		);
+		$this->document = new DOMDocument('1.0', 'UTF-8');
 	}
 
+	//TODO: rewrite from other side (one sided relations) of link entity metadata
 	public function actionView(int $id = NULL)
 	{
-		$this->properties = array_combine(
-			array_map(
-				'crc32',
-				array_column(
-					$this->properties = array_filter(
-						$this->linkRepository->getEntityMetadata()->getProperties(),
-						function (Nextras\Orm\Entity\Reflection\PropertyMetadata $propertyMetadata) {
-							return $propertyMetadata->relationship && $propertyMetadata->relationship->type === Nextras\Orm\Entity\Reflection\PropertyRelationshipMetadata::ONE_HAS_ONE;
-						}
-					),
-					'name'
-				)
-			),
-			$this->properties
-		);
+		$this->properties = array_combine(array_map('crc32', array_column($this->properties = array_filter($this->linkRepository->getEntityMetadata()->getProperties(), function (Nextras\Orm\Entity\Reflection\PropertyMetadata $propertyMetadata) {
+			return $propertyMetadata->relationship && $propertyMetadata->relationship->type === Nextras\Orm\Entity\Reflection\PropertyRelationshipMetadata::ONE_HAS_ONE;
+		}), 'name')), $this->properties);
 		$id === NULL ? $this->generateIndex() : $this->generateUrlSet($id);
 	}
 
@@ -84,10 +70,7 @@ final class Presenter
 			$this->error();
 		}
 
-		return new Ytnuk\Orm\Pagination\Control(
-			$this->model->getRepository($this->property->relationship->repository)->findAll(),
-			self::ITEMS_PER_PAGE
-		);
+		return new Ytnuk\Orm\Pagination\Control($this->model->getRepository($this->property->relationship->repository)->findAll(), self::ITEMS_PER_PAGE);
 	}
 
 	private function generateIndex()
@@ -96,51 +79,27 @@ final class Presenter
 		if ( ! $domain instanceof Ytnuk\Web\Domain\Entity) {
 			$this->error();
 		}
-		$index = $this->document->appendChild(
-			$this->document->createElementNS(
-				self::NAMESPACE_URI,
-				'sitemapindex'
-			)
-		);
-		array_walk(
-			$this->properties,
-			function (
-				Nextras\Orm\Entity\Reflection\PropertyMetadata $metadata,
-				int $id
-			) use
-			(
-				$index
+		$index = $this->document->appendChild($this->document->createElementNS(self::NAMESPACE_URI, 'sitemapindex'));
+		array_walk($this->properties, function (
+			Nextras\Orm\Entity\Reflection\PropertyMetadata $metadata,
+			int $id
+		) use
+		(
+			$index
+		) {
+			$this->property = $metadata;
+			$this->params['id'] = $id;
+			$pagination = $this['pagination'];
+			$paginator = $pagination->getPaginator();
+			foreach (
+				range($paginator->getFirstPage(), $paginator->getLastPage()) as $page
 			) {
-				$this->property = $metadata;
-				$this->params['id'] = $id;
-				$pagination = $this['pagination'];
-				$paginator = $pagination->getPaginator();
-				foreach (
-					range(
-						$paginator->getFirstPage(),
-						$paginator->getLastPage()
-					) as $page
-				) {
-					$sitemap = $index->appendChild($this->document->createElement('sitemap'));
-					$sitemap->appendChild(
-						$this->document->createElement(
-							'loc',
-							$pagination->link(
-								'//this',
-								['page' => $page]
-							)
-						)
-					);
-					$sitemap->appendChild(
-						$this->document->createElement(
-							'lastmod',
-							(new DateTime)->format('c')
-						)
-					);
-				}
-				unset($this['pagination']); //mischief managed
+				$sitemap = $index->appendChild($this->document->createElement('sitemap'));
+				$sitemap->appendChild($this->document->createElement('loc', $pagination->link('//this', ['page' => $page])));
+				$sitemap->appendChild($this->document->createElement('lastmod', (new DateTime)->format('c')));
 			}
-		);
+			unset($this['pagination']); //mischief managed
+		});
 	}
 
 	private function generateUrlSet(
@@ -149,38 +108,17 @@ final class Presenter
 		if ( ! $this->property = $this->properties[$id] ?? NULL) {
 			$this->error();
 		}
-		$urlSet = $this->document->appendChild(
-			$this->document->createElementNS(
-				self::NAMESPACE_URI,
-				'urlset'
-			)
-		);
+		$urlSet = $this->document->appendChild($this->document->createElementNS(self::NAMESPACE_URI, 'urlset'));
 		$collection = iterator_to_array($this['pagination']);
-		array_walk(
-			$collection,
-			function (Nextras\Orm\Entity\IEntity $entity) use
-			(
-				$urlSet
-			) {
-				//TODO: use parent cache to determine lastmod for whole sitemap
-				//TODO: cache each url element, so lastmod will always be up to date
-				$url = $urlSet->appendChild($this->document->createElement('url'));
-				$url->appendChild(
-					$this->document->createElement(
-						'loc',
-						$this->link(
-							$entity->getValue($this->property->relationship->property),
-							['absolute' => TRUE]
-						)
-					)
-				);
-				$url->appendChild(
-					$this->document->createElement(
-						'lastmod',
-						(new DateTime)->format('c')
-					)
-				);
-			}
-		);
+		array_walk($collection, function (Nextras\Orm\Entity\IEntity $entity) use
+		(
+			$urlSet
+		) {
+			//TODO: use parent cache to determine lastmod for whole sitemap
+			//TODO: cache each url element, so lastmod will always be up to date
+			$url = $urlSet->appendChild($this->document->createElement('url'));
+			$url->appendChild($this->document->createElement('loc', $this->link($entity->getValue($this->property->relationship->property), ['absolute' => TRUE])));
+			$url->appendChild($this->document->createElement('lastmod', (new DateTime)->format('c')));
+		});
 	}
 }

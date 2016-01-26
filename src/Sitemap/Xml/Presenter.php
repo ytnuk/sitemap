b2a6f4b783a3d93458c3388aa6ec419ed6d0be70
Vít Kutný <vitkutny@icloud.com>
2016-01-26 20:40:33 +0100

    refactor

--- src/Sitemap/Xml/Presenter.php
+++ src/Sitemap/Xml/Presenter.php
@@ -15,24 +15,14 @@ final class Presenter
 	const NAMESPACE_URI = 'http://www.sitemaps.org/schemas/sitemap/0.9';
 
 	/**
-	 * @var Nextras\Orm\Model\IModel
+	 * @var Nette\DI\Container
 	 */
-	private $model;
+	private $container;
 
 	/**
-	 * @var Ytnuk\Link\Repository
+	 * @var Nextras\Orm\Repository\IRepository
 	 */
-	private $linkRepository;
-
-	/**
-	 * @var array|Nextras\Orm\Entity\Reflection\PropertyMetadata[]
-	 */
-	private $properties = [];
-
-	/**
-	 * @var Nextras\Orm\Entity\Reflection\PropertyMetadata
-	 */
-	private $property;
+	private $repository;
 
 	/**
 	 * @var DOMDocument
@@ -40,22 +30,36 @@ final class Presenter
 	private $document;
 
 	public function __construct(
-		Nextras\Orm\Model\IModel $model,
-		Ytnuk\Link\Repository $linkRepository
+		Nette\DI\Container $container
 	) {
 		parent::__construct();
-		$this->model = $model;
-		$this->linkRepository = $linkRepository;
+		$this->container = $container;
 		$this->document = new DOMDocument('1.0', 'UTF-8');
 	}
 
-	//TODO: rewrite from other side (one sided relations) of link entity metadata
+	private function getLinkProperties(Nextras\Orm\Repository\IRepository $repository)
+	{
+		return array_filter($repository->getEntityMetadata()->getProperties(), function (Nextras\Orm\Entity\Reflection\PropertyMetadata $propertyMetadata) {
+			return $propertyMetadata->relationship && ($propertyMetadata->relationship->entity === Ytnuk\Link\Entity::class || is_subclass_of($propertyMetadata->relationship->entity, Ytnuk\Link\Entity::class)) && $propertyMetadata->relationship->type === Nextras\Orm\Entity\Reflection\PropertyRelationshipMetadata::ONE_HAS_ONE;
+		});
+	}
+
 	public function actionView(int $id = NULL)
 	{
-		$this->properties = array_combine(array_map('crc32', array_column($this->properties = array_filter($this->linkRepository->getEntityMetadata()->getProperties(), function (Nextras\Orm\Entity\Reflection\PropertyMetadata $propertyMetadata) {
-			return $propertyMetadata->relationship && $propertyMetadata->relationship->type === Nextras\Orm\Entity\Reflection\PropertyRelationshipMetadata::ONE_HAS_ONE;
-		}), 'name')), $this->properties);
-		$id === NULL ? $this->generateIndex() : $this->generateUrlSet($id);
+		$repositories = array_combine(array_map('crc32', array_map('get_class', $repositories = array_map([
+			$this->container,
+			'getService',
+		], $this->container->findByType(Nextras\Orm\Repository\IRepository::class)))), $repositories);
+		if ($id === NULL) {
+			$this->generateIndex(array_filter($repositories, [
+				$this,
+				'getLinkProperties',
+			]));
+		} elseif (isset($repositories[$id])) {
+			$this->generateUrlSet($this->repository = $repositories[$id]);
+		} else {
+			$this->error();
+		}
 	}
 
 	public function renderView()
@@ -66,28 +70,28 @@ final class Presenter
 
 	protected function createComponentPagination() : Ytnuk\Orm\Pagination\Control
 	{
-		if ( ! $this->property) {
+		if ( ! $this->repository) {
 			$this->error();
 		}
 
-		return new Ytnuk\Orm\Pagination\Control($this->model->getRepository($this->property->relationship->repository)->findAll(), self::ITEMS_PER_PAGE);
+		return new Ytnuk\Orm\Pagination\Control($this->repository->findAll(), self::ITEMS_PER_PAGE);
 	}
 
-	private function generateIndex()
+	private function generateIndex(array $repositores)
 	{
 		$domain = $this->web->domains->get()->getBy(['host' => $this->domain]);
 		if ( ! $domain instanceof Ytnuk\Web\Domain\Entity) {
 			$this->error();
 		}
 		$index = $this->document->appendChild($this->document->createElementNS(self::NAMESPACE_URI, 'sitemapindex'));
-		array_walk($this->properties, function (
-			Nextras\Orm\Entity\Reflection\PropertyMetadata $metadata,
+		array_walk($repositores, function (
+			Nextras\Orm\Repository\IRepository $repository,
 			int $id
 		) use
 		(
 			$index
 		) {
-			$this->property = $metadata;
+			$this->repository = $repository;
 			$this->params['id'] = $id;
 			$pagination = $this['pagination'];
 			$paginator = $pagination->getPaginator();
@@ -102,23 +106,31 @@ final class Presenter
 		});
 	}
 
-	private function generateUrlSet(
-		int $id
-	) {
-		if ( ! $this->property = $this->properties[$id] ?? NULL) {
+	private function generateUrlSet(Nextras\Orm\Repository\IRepository $repository)
+	{
+		if ( ! $properies = $this->getLinkProperties($repository)) {
 			$this->error();
 		}
 		$urlSet = $this->document->appendChild($this->document->createElementNS(self::NAMESPACE_URI, 'urlset'));
 		$collection = iterator_to_array($this['pagination']);
 		array_walk($collection, function (Nextras\Orm\Entity\IEntity $entity) use
 		(
-			$urlSet
+			$properies,
+			$urlSet,
+			$repository
 		) {
 			//TODO: use parent cache to determine lastmod for whole sitemap
 			//TODO: cache each url element, so lastmod will always be up to date
-			$url = $urlSet->appendChild($this->document->createElement('url'));
-			$url->appendChild($this->document->createElement('loc', $this->link($entity->getValue($this->property->relationship->property), ['absolute' => TRUE])));
-			$url->appendChild($this->document->createElement('lastmod', (new DateTime)->format('c')));
+			array_walk($properies, function (Nextras\Orm\Entity\Reflection\PropertyMetadata $propertyMetadata) use
+			(
+				$entity,
+				$urlSet,
+				$repository
+			) {
+				$url = $urlSet->appendChild($this->document->createElement('url'));
+				$url->appendChild($this->document->createElement('loc', $this->link($entity->getValue($propertyMetadata->name), ['absolute' => TRUE])));
+				$url->appendChild($this->document->createElement('lastmod', (new DateTime)->format('c')));
+			});
 		});
 	}
 }
